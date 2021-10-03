1d9b0390a8f289175776904d62dacdc9a3cd9501
Karl Heinz Marbaise <kama@soebes.de>
2021-10-03 23:48:04 +0200

    First.

---
+++ src/main/java/com/soebes/duplicate/PomTestProjects.java
@@ -0,0 +1,18 @@
+package com.soebes.duplicate;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class PomTestProjects {
+
+  public static void main(String[] args) throws IOException {
+
+    Model model = new Model();
+    MavenXpp3Writer mavenXpp3Writer = new MavenXpp3Writer();
+    mavenXpp3Writer.write(new FileOutputStream(new File("pom.xml")), model);
+  }
+}
