6d8130e45d2690de197434cb78dc6e1df0884329
Karl Heinz Marbaise <kama@soebes.de>
2021-11-26 00:25:58 +0100

    Setting.xml setup. Cloning testing repo. Testing 1. step.

--- testing.sh
+++ testing.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 URL="https://repo1.maven.org/maven2/org/apache/maven/apache-maven"
-VERSIONS=('3.0.5' '3.1.1' '3.2.5' '3.3.9' '3.5.4' '3.6.3' '3.8.1' '3.8.2' '3.8.3' '3.8.4' '4.0.0-alpha-1-SNAPSHOT')
+VERSIONS=('3.0.5' '3.1.1' '3.2.5' '3.3.9' '3.5.4' '3.6.3' '3.8.1' '3.8.2' '3.8.3' '3.8.4')
+#VERSIONS=('3.8.1' '3.8.2' '3.8.3' '3.8.4' '4.0.0-alpha-1-SNAPSHOT')
 
 for i in "${VERSIONS[@]}"; do
 	if [ -f "apache-maven-$i-bin.tar.gz" ]; then
@@ -18,15 +19,16 @@ for i in "${VERSIONS[@]}"; do
 	fi
 done
 
-COMMAND=()
-for i in "${VERSIONS[@]}"; do
-	COMMAND+=( "../apache-maven-$i/bin/mvn clean" )
-done
-
+#COMMAND=()
+#for i in "${VERSIONS[@]}"; do
+#	COMMAND+=( "../apache-maven-$i/bin/mvn clean" )
+#done
+#
 cd reactor
 
 ###hyperfine --warmup 5 --export-markdown ../results.md --parameter-list version "${versions[*]}" '../apache-maven-{version}/bin/mvn clean'
-## ( IFS=, ; hyperfine -w 5 --export-markdown ../results-$jdk.md --parameter-list version "${VERSIONS[*]}" -n "$jdk-{version}" '../apache-maven-{version}/bin/mvn clean' )
+  ## ( IFS=, ; hyperfine -w 5 --export-markdown ../results-$jdk.md --parameter-list version "${VERSIONS[*]}" -n "$jdk-{version}" '../apache-maven-{version}/bin/mvn clean' )
+#hyperfine --warmup 1 --export-markdown ../results.md "${COMMAND[@]}"
 
-hyperfine --warmup 5 --export-markdown ../results.md "${COMMAND[@]}"
+( IFS=, ; hyperfine -w 5 --export-markdown ../results-$jdk.md -L VERSION "${VERSIONS[*]}" -n "$jdk-apache-maven-{VERSION}" '../apache-maven-{VERSION}/bin/mvn clean' )
 cd ..
