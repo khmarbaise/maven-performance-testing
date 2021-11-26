3a6dfccd810e02164b2f087a34cab55a954c9696
Karl Heinz Marbaise <kama@soebes.de>
2021-11-26 23:44:58 +0100

    Improved.

--- run.sh
+++ run.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
+URL="https://repo1.maven.org/maven2/org/apache/maven/apache-maven"
+APACHE_MAVEN_VERSION="3.8.4"
+APACHE_MAVEN="apache-maven-${APACHE_MAVEN_VERSION}-bin.tar.gz"
+
+if [ -f "${APACHE_MAVEN}" ]; then
+  echo "File ${APACHE_MAVEN} already downloaded."
+else
+  wget $URL/${APACHE_MAVEN_VERSION}/${APACHE_MAVEN}
+fi
+# Unpack
+if [ -d "apache-maven-${APACHE_MAVEN_VERSION}" ]; then
+  echo "Directory apache-maven-${APACHE_MAVEN_VERSION} exists."
+else
+  tar -zxf $APACHE_MAVEN.tar.gz
+fi
+
+source ~/.sdkman/bin/sdkman-init.sh
 
 mkdir -p src/site/markdown
 mkdir -p /var/www/soebes.team/site/
 
-sdk install java 8.0.302-open
+sdk use java 8.0.302-open
 ./testing.sh JDK8
 
-sdk install java 11.0.12-open
+sdk use java 11.0.12-open
 ./testing.sh JDK11
 
-sdk install java 17.0.1-open
+sdk use java 17.0.1-open
 ./testing.sh JDK17
 
-./apache-maven-3.8.3/bin/mvn clean site
-./apache-maven-3.8.3/bin/mvn site:stage -DstagingDirectory=/var/www/soebes.team/site/
+./apache-maven-${APACHE_MAVEN_VERSION}/bin/mvn clean site
+./apache-maven-${APACHE_MAVEN_VERSION}/bin/mvn site:stage -DstagingDirectory=/var/www/soebes.team/site/
--- testing.sh
+++ testing.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
-JDK=$1
 URL="https://repo1.maven.org/maven2/org/apache/maven/apache-maven"
-VERSIONS=('3.0.5')
+# Define the versions we would like to test.
 #VERSIONS=('3.0.5' '3.1.1' '3.2.5' '3.3.9' '3.5.4' '3.6.3' '3.8.1' '3.8.2' '3.8.3' '3.8.4')
-#VERSIONS=('3.8.1' '3.8.2' '3.8.3' '3.8.4' '4.0.0-alpha-1-SNAPSHOT')
+VERSIONS=('3.0.5' '3.8.4')
+JDK=$1
 
 for i in "${VERSIONS[@]}"; do
 	if [ -f "apache-maven-$i-bin.tar.gz" ]; then
@@ -21,16 +21,7 @@ for i in "${VERSIONS[@]}"; do
 	fi
 done
 
-#COMMAND=()
-#for i in "${VERSIONS[@]}"; do
-#	COMMAND+=( "../apache-maven-$i/bin/mvn clean" )
-#done
-#
+
 cd reactor
-
-###hyperfine --warmup 5 --export-markdown ../results.md --parameter-list version "${versions[*]}" '../apache-maven-{version}/bin/mvn clean'
-  ## ( IFS=, ; hyperfine -w 5 --export-markdown ../results-$jdk.md --parameter-list version "${VERSIONS[*]}" -n "$jdk-{version}" '../apache-maven-{version}/bin/mvn clean' )
-#hyperfine --warmup 1 --export-markdown ../results.md "${COMMAND[@]}"
-
-( IFS=, ; hyperfine -w 5 --export-markdown ../src/site/markdown/results-$jdk.md -L VERSION "${VERSIONS[*]}" -n 'apache-maven-{VERSION}' '../apache-maven-{VERSION}/bin/mvn clean' )
+( IFS=, ; hyperfine -w 5 --export-markdown ../src/site/markdown/results-JDK.md -L VERSION "${VERSIONS[*]}" -n '{VERSION}' '../apache-maven-{VERSION}/bin/mvn clean' )
 cd ..
