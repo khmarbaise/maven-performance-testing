63b83e4a3cd7efc44e57276c69d1bcc45d159291
Karl Heinz Marbaise <kama@soebes.de>
2023-02-09 00:11:48 +0100

    Clean up.

--- src/main/java/com/soebes/maven/performance/sdkman/SDKManJDKS.java
+++ src/main/java/com/soebes/maven/performance/sdkman/SDKManJDKS.java
@@ -24,7 +24,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.function.Predicate.not;
