5bb36b2094498e283f51c5868424902fb85a4306
Karl Heinz Marbaise <kama@soebes.de>
2023-06-09 16:59:47 +0200

    Improved.

--- graphics/src/main/java/com/soebes/maven/performance/Selector.java
+++ graphics/src/main/java/com/soebes/maven/performance/Selector.java
@@ -6,10 +6,10 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Predicate;
 
-class Selector {
-  private static final Predicate<Path> IS_REGULAR_FILE = Files::isRegularFile;
-  private static final Predicate<Path> IS_READABLE = Files::isReadable;
-  private static final Predicate<Path> IS_VALID_FILE = IS_REGULAR_FILE.and(IS_READABLE);
+interface Selector {
+  Predicate<Path> IS_REGULAR_FILE = Files::isRegularFile;
+  Predicate<Path> IS_READABLE = Files::isReadable;
+  Predicate<Path> IS_VALID_FILE = IS_REGULAR_FILE.and(IS_READABLE);
 
   static List<Path> selectAllFiles(Path start) throws IOException {
     try (var pathStream = Files.walk(start, 1)) {
@@ -18,8 +18,7 @@ class Selector {
   }
 
   static List<Path> selectJsonFilesOnly(Path start) throws IOException {
-    var paths = selectAllFiles(start);
-    return  paths.stream()
+    return  selectAllFiles(start).stream()
         .filter(s -> s.getFileName().toString().endsWith(".json"))
         .toList();
   }
