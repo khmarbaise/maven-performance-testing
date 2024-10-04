20222a14e07e8efda546769ef3c703fcb36b5135
Karl Heinz Marbaise <kama@soebes.de>
2024-10-04 21:06:35 +0200

    Changed to use Maven 3.9.9 Changed to use different JDK's

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ source ~/.sdkman/bin/sdkman-init.sh
 sdk install maven 3.8.8
 
 # TODO: Maybe we should precompile the tool via GraalVM?
-sdk use java 17.0.9-tem
+sdk use java 17.0.12-tem
 
 cd /home/tmpt/maven-performance-testing
 #
